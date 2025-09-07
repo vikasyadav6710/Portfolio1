a06c6684fc9e24d3a97863778fa64f3c6b299e99
vikasyadav6710 <cse.14bcs1603@gmail.com>
2025-09-07 16:29:26 +0530

    vite comfig

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    allowedHosts: [
+      'localhost', // Often included by default
+      '127.0.0.1', // Often included by default
+      'devserver-main--rao-portfolio.netlify.app',
+      'rao-portfolio.netlify.app', // Add the specific host here
+    ]
+  }
 })
